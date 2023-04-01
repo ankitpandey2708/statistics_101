413e4957a0b8cbb2c9ddf21069ffea5a808bb8a8
Ankit Pandey <ankitpandey2708@gmail.com>
2023-04-01 18:22:17 +0530

    either use boxplot or histogram

--- outliers.py
+++ outliers.py
@@ -17,8 +17,7 @@ lower_bound = q1 - (1.5 * iqr)
 outliers = [x for x in data if x < lower_bound or x > upper_bound]
 
 
-#chart = sns.boxplot(data=data)
-#chart.axhline(mean, color='r', linestyle='--')
+#chart = sns.boxplot(data=data) #chart.axhline(mean, color='r', linestyle='--')
 plt.hist(data, bins=30, alpha=0.5)
 plt.axvline(median, color='red', linestyle='dashed', linewidth=2)
 plt.axvline(mean, color='green', linestyle='dashed', linewidth=2)
