fd5d5d23cf4249cf7766fd1637de336ff6cde638
Ankit Pandey <ankitpandey2708@gmail.com>
2023-04-01 18:41:00 +0530

    Update outliers.py

--- outliers.py
+++ outliers.py
@@ -17,10 +17,13 @@ lower_bound = q1 - (1.5 * iqr)
 outliers = [x for x in data if x < lower_bound or x > upper_bound]
 
 
-#chart = sns.boxplot(data=data) #chart.axhline(mean, color='r', linestyle='--')
+plt.boxplot(data)
+plt.axhline(mean, color='r', linestyle='--')
+'''
 plt.hist(data, bins=30, alpha=0.5)
 plt.axvline(median, color='red', linestyle='dashed', linewidth=2)
 plt.axvline(mean, color='green', linestyle='dashed', linewidth=2)
+'''
 
 # Add labels and title
 plt.xlabel('')
